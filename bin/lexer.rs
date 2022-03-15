c8a6a5c5ab08a88711fc1e79622547ee9b8f04dd
Hippo Spark (Arch) <hippospark@gmail.com>
2022-03-15 17:43:11 +0800

    添加了 lexer 的命令程序.

--- bin/lexer.rs
+++ bin/lexer.rs
@@ -1,3 +1,5 @@
+use std::{env, fs, process};
+
 /**
  * Copyright (c) 2022 Hemashushu <hippospark@gmail.com>, All rights reserved.
  *
@@ -5,9 +7,28 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-
 use front_end::lexer;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        println!(
+            "\
+usage:
+
+$ cargo run --bin lexer path_to_script_file
+e.g.
+$ cargo run --bin lexer scripts/01-base-expression.xuan"
+        );
+        process::exit(1);
+    }
+
+    let file_path = &args[1];
+    let program = fs::read_to_string(file_path).expect("read file error");
+    let result = lexer::tokenize(&program).expect("tokenize error");
+
+    for token in result {
+        println!("{:?}", token);
+    }
 }
